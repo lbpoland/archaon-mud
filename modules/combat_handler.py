040c5ac272f15ea052d9ccf09af3f08942776df9
lbpoland <lbpoland8904@gmail.com>
2025-03-03 02:43:49 +1000

    Update combat_handler.py

--- modules/combat_handler.py
+++ modules/combat_handler.py
@@ -9,7 +9,7 @@
 
 import random
 from math import log, sqrt
-from modules.skills_handler import Player
+from modules.skills_handler import Player, COLOUR
 from modules.deities import DEITIES
 
 # Comprehensive combat data
